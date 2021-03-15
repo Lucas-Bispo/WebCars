6ac834b3b5640c8171882a5b0267759ea33ce766
Lucas-Bispo <lukaobispo@gmail.com>
2021-03-14 23:54:35 -0300

    bloc evento add

--- lib/src/bloc/login_component/login_component_event.dart
+++ lib/src/bloc/login_component/login_component_event.dart
@@ -1,4 +1,20 @@
 part of 'login_component_bloc.dart';
 
 @immutable
-abstract class LoginComponentEvent {}
+abstract class LoginComponentEvent {
+  final String _usuario;
+  
+  final String _senha;
+
+  const LoginComponentEvent(this._usuario, this._senha);
+
+  @override
+  List<Object> get props => [_usuario, _senha];    
+}
+
+class LoginComponentEventLogar extends LoginComponentEvent {
+  const LoginComponentEventLogar(usuario, senha) : super(usuario, senha);
+
+  @override
+  List<Object> get props => [_usuario, _senha];    
+}
